3a8a0fe35e90f3cdc468cd2359c959f7c7c7830d
skorskip <skorskip@umich.edu>
2020-04-05 14:50:37 -0400

    Remove more unused services

--- src/app/data-models/game/game.service.ts
+++ src/app/data-models/game/game.service.ts
@@ -1,29 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
 import { Observable, of } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
-
 import { Game } from './game';
-import { environment } from '../../../environments/environment';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({ providedIn: 'root' })
 export class GameService {
-  private gamesUrl = environment.serviceURL + 'games';  // URL to web api
-
   constructor(
-    private http: HttpClient,
     private snackBar: MatSnackBar) { }
-
-  // /** GET game by id. Will 404 if id not found */
-  getGame(id: number): Observable<Game> {
-    const url = `${this.gamesUrl}/${id}`;
-    return this.http.get<Game>(url).pipe(
-      tap(_ => console.log(`fetched game id=${id}`)),
-      catchError(this.handleError<Game>(`getGame id=${id}`))
-    );
-  }
 
   getGameLocal(id: number, games: Game[]): Game {
     var game
